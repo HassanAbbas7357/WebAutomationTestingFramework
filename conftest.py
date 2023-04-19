57fceed8f7308c519c65534ad11f618191b8c2e8
Hassan Abbas <ah735752@gmail.com>
2023-04-19 16:16:29 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -96,7 +96,8 @@ def driver(request):
         chrome_options = Chrome_options()
         chrome_options.add_argument(headless)
         chrome_options.add_argument('--log-level=3')
-        chrome_options.add_argument("start-maximized")
+        #chrome_options.add_argument("start-maximized")
+        chrome_options.add_argument('--start-maximized')
         chrome_options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.notifications": 1,
             "download.default_directory": downloadsPath,

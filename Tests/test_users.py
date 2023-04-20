1efc2582351bcd0e58df7a26254ac4e068876717
Hassan-Abbas-SQA <49572299+HassanAbbas7357@users.noreply.github.com>
2023-04-20 13:10:04 +0500

    added Users TestCases

--- Tests/test_users.py
+++ Tests/test_users.py
@@ -1,6 +1,6 @@
 from Locators.TempWorkersLocators import csvFileEXPORTNAME, nextPage, mainShowHideButton, mainExportButton, \
     mainFilterButton, mainRowRadioButton, mainDeleteButton, mainDeleteYesButton, \
-    mainTemporaryDelete_SuccessMessage_xpath
+    mainTemporaryDelete_SuccessMessage_xpath, TableLoader_xpath
 from Tests.test_temprory_worker import downloadAndVerifyCSVExportedFile, sortTableByIdDescendingOrder
 from conftest import *
 from base_helpers import *
@@ -17,10 +17,9 @@ def test_UsersScreenNavigation(driver):
     loginwithSteps(driver)
     UsersUrl = 'active-user'
     with allure.step('Then User clicks "Users" button on "Homepage" screen'):
-        scroll_into_element(UsersMainLink_xpath,driver)
-        click_on_element_js(UsersMainLink_xpath, driver)
+        scroll_into_element(UsersMainLink_xpath, driver)
+        find_byXpath(UsersMainLink_xpath, driver).click()
     with allure.step('And User Should see "active-user" in the url'):
-        print(driver.current_url)
         assert UsersUrl in driver.current_url
 
 
@@ -49,7 +48,9 @@ def test_UsersCheckExportButtonDownloadsCSVFile(driver):
 def test_UsersVerifyIfExportedCSVFileDataMatchesWithWebTableData(driver):
     VisitUsersPageWithLogin(driver)
     with allure.step('And Check if CSV file downloaded correctly'):
-        fileName = Storage.downloadsPath + find_byXpath(csvFileEXPORTNAME, driver).get_attribute("download") + '.csv'
+        fileName = Storage.downloadsPath + \
+            find_byXpath(csvFileEXPORTNAME, driver).get_attribute(
+                "download") + '.csv'
         downloadAndVerifyCSVExportedFile(driver)
 
     with allure.step("Verify If Exported CSV file has the exact data matches with table"):
@@ -79,10 +80,12 @@ def test_UsersVerifyIfExportedCSVFileDataMatchesWithWebTableData(driver):
             approve_by = csv_list[7]
             status = csv_list[8]
 
-            listOfData = [ID, first_name, last_name, email, role, phone_number, created_by, approve_by, status]
+            listOfData = [ID, first_name, last_name, email, role,
+                          phone_number, created_by, approve_by, status]
 
             # noinspection PyTypeChecker
-            verifyDatainTableByRow(driver, rowData=listOfData, rowIndex=counter)
+            verifyDatainTableByRow(
+                driver, rowData=listOfData, rowIndex=counter)
             counter += 1
 
 
@@ -137,6 +140,7 @@ def test_UserCheckNecessaryButtonsVisible(driver):
 def test_UserDeleteLastAndVerify(driver):
     VisitUsersPageWithLogin(driver)
     with allure.step('When User Tap on ID on table to sort it by descending order'):
+        verify_loaderAndWait(TableLoader_xpath, driver)
         sortTableByIdDescendingOrder(driver)
     with allure.step('And User select the last Item and Tap on Delete Button'):
         find_byXpath(deleteUser_xpath, driver).click()
@@ -154,6 +158,7 @@ def test_UserDeleteLastAndVerify(driver):
 def test_UserDeleteFirstAndVerify(driver):
     VisitUsersPageWithLogin(driver)
     with allure.step('And User select the last Item and Tap on Delete Button'):
+        verify_loaderAndWait(TableLoader_xpath, driver)
         find_byXpathAndWait(deleteUser_xpath, driver).click()
         find_byXpathAndWait(mainDeleteYesButton, driver).click()
     with allure.step('Then User should see a "User Deleted Successfully" message'):
@@ -184,10 +189,119 @@ def test_verifyEditFormFieldsArePresent(driver):
 def test_verifyEditFormShowingChangesSavedSuccessWhenSubmittingFormWithoutChanges(driver):
     VisitUsersPageWithLogin(driver)
     with allure.step('And User Click on Edit Button'):
+        verify_loaderAndWait(TableLoader_xpath, driver)
         find_byXpathAndWait(userEditBtn_xpath, driver).click()
     with allure.step('Then Click On Save Changes button without even changing anything"'):
         find_byXpathAndWait(saveChangesButton, driver).click()
-    with allure.step('And User Should see an error "Invalid User"'):
-        verify_visibility_of_element_located(invalidUserMessage,driver)
+    with allure.step('And User Should see Success Message "User Updated Successfully"'):
+        verify_visibility_of_element_located(invalidUserMessage, driver)
 
 
+@allure.feature("Users Feature")
+@allure.story("Edit the FirstName and Verify in Table")
+@allure.severity(allure.severity_level.MINOR)
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_EditFirstNameAndVerifyInTable(driver):
+    VisitUsersPageWithLogin(driver)
+    FirstName = get_randomEmail().replace('@gmail.com', '')
+    with allure.step('And User Click on Edit Button'):
+        verify_loaderAndWait(TableLoader_xpath, driver)
+        find_byXpathAndWait(userEditBtn_xpath, driver).click()
+    with allure.step('Then Click On Save Changes button after changing FirstName"'):
+        driver.execute_script(
+            "arguments[0].value='';", find_byXpath(firstNameInput, driver))
+        find_byXpath(firstNameInput, driver).send_keys(FirstName)
+        print(FirstName)
+        find_byXpathAndWait(saveChangesButton, driver).click()
+    with allure.step('And User Should see Success Message "User Updated SuccessFully"'):
+        verify_visibility_of_element_located(invalidUserMessage, driver)
+        sleep(3)
+    with allure.step('Then Verify FirstName Updated in the Table'):
+        Name = find_Elements_byXpathAndWait_getText(firstRow, driver)
+        print(Name)
+        assert FirstName == Name[1]
+
+
+@allure.feature("Users Feature")
+@allure.story("Edit the LastName and Verify in Table")
+@allure.severity(allure.severity_level.MINOR)
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_EditLastNameAndVerifyInTable(driver):
+    VisitUsersPageWithLogin(driver)
+    FirstName = get_randomEmail().replace('@gmail.com', '')
+    with allure.step('And User Click on Edit Button'):
+        verify_loaderAndWait(TableLoader_xpath, driver)
+        find_byXpathAndWait(userEditBtn_xpath, driver).click()
+    with allure.step('Then Click On Save Changes button after changing LastName"'):
+        driver.execute_script(
+            "arguments[0].value='';", find_byXpath(lastNameInput, driver))
+        find_byXpath(lastNameInput, driver).send_keys(FirstName)
+        print(FirstName)
+        find_byXpathAndWait(saveChangesButton, driver).click()
+    with allure.step('And User Should see Success Message "User Updated SuccessFully"'):
+        verify_visibility_of_element_located(invalidUserMessage, driver)
+        sleep(3)
+    with allure.step('Then Verify LastName Updated in the Table'):
+        Name = find_Elements_byXpathAndWait_getText(firstRow, driver)
+        print(Name)
+        assert FirstName == Name[2]
+
+
+def visitAddUserScreenWithLoginSteps(driver):
+    VisitUsersPageWithLogin(driver)
+    AddUserUrl = '/user/add'
+    with allure.step('And User Click on Add User Button'):
+        # verify_loaderAndWait(TableLoader_xpath, driver)
+        scroll_to_bottom_of_page(driver)
+        find_byXpath(addNewUserMain_xpath, driver).click()
+    with allure.step('And Verify User is on UserAdd Screen'):
+        assert AddUserUrl in driver.current_url
+
+
+@allure.feature("Users Feature")
+@allure.story("Add New User Screen Click Submit Button And Verify Errors")
+@allure.severity(allure.severity_level.MINOR)
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_AddAndVerifyErrors(driver):
+    visitAddUserScreenWithLoginSteps(driver)
+
+    with allure.step('When User Clicks on AddUser Button'):
+        find_byXpath(AddUserButton, driver).click()
+    with allure.step('Then User Should See Please Input message for all the required fields'):
+        verify_visibility_of_element_located("//div[@class='ant-form-item-explain-error' and text()='Please input "
+                                             "your first name!']", driver)
+        verify_visibility_of_element_located("//div[@class='ant-form-item-explain-error' and text()='Please input "
+                                             "your last name']", driver)
+        verify_visibility_of_element_located("//div[@class='ant-form-item-explain-error' and text()='Please input "
+                                             "your phone number!']", driver)
+        verify_visibility_of_element_located("//div[@class='ant-form-item-explain-error' and text()='Please Enter "
+                                             "Your Email!']", driver)
+        verify_visibility_of_element_located("//div[@class='ant-form-item-explain-error' and text()='Please Select "
+                                             "user role!']", driver)
+
+
+@allure.feature("Users Feature")
+@allure.story("Add New User And Verify In Table")
+@allure.severity(allure.severity_level.MINOR)
+@pytest.mark.regression
+@pytest.mark.sanity
+def test_AddNewUserAndVerify(driver):
+    visitAddUserScreenWithLoginSteps(driver)
+
+    with allure.step('When User Inputs Fills Form with Valid Data'):
+        find_byXpath(firstNameInput, driver).send_keys("Bot")
+        find_byXpath(lastNameInput, driver).send_keys("Hassan")
+        find_byXpath(Email_xpath, driver).send_keys(get_randomEmail())
+        find_byXpath(phoneInput, driver).send_keys("+923163466125")
+        find_byXpath(roleClickField, driver).click()
+        find_byXpathAndWait(selectAdmin, driver).click()
+        find_byXpath(inActiveRadioBtn, driver).click()
+
+    with allure.step('When User Clicks on AddUser Button'):
+        find_byXpath(AddUserButton, driver).click()
+
+    with allure.step('Then User Should see "User Added Successfully" message'):
+        verify_visibility_of_element_located(userAddedMessage, driver)
